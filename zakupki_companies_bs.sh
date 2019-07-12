d11a783366b614c1e1e8469b996b9744360c24cf
Vasiliy V. Garshin <50753438+vgarshin@users.noreply.github.com>
2019-07-12 17:57:39 +0300

    Update zakupki_companies_bs.sh

--- zakupki_companies_bs.sh
+++ zakupki_companies_bs.sh
@@ -4,15 +4,26 @@ cd `dirname "$0"`
 
 VOLUMES="-v /home/user/extradata:/home/user/extradata"
 PROC_DATE=`date +%Y%m%d`
-CACHE_DIR="cache_zakupki_companies_bs"
+CACHE_DIR="cache_zakupki_companies_rng"
 DATA_DIR="_data"
 EMAIL="vgarshin@yandex.ru"
-CONT_NAME="parcesites_zakupki"
+CONT_NAME="parcesites_zakupki_rng"
+RNUM_START=19000000
+RNUM_END=19000300
+RNUM_STEP=100
 
-sudo mkdir "../$CACHE_DIR"
+for ((RNUM=RNUM_START; RNUM<RNUM_END; RNUM+=RNUM_STEP))
+	do
+		RNUM_END_STEP=$(($RNUM+$RNUM_STEP))
+		echo "collecting reestr numbers from $RNUM to $RNUM_END_STEP"
+		
+		sudo mkdir "../$CACHE_DIR"
 
-sudo docker run -i --name $CONT_NAME $VOLUMES extradata python zakupki/zakupki_companies_bs.py $DATA_DIR $PROC_DATE $CACHE_DIR $EMAIL
-sudo docker rm -f $CONT_NAME
+		sudo docker run -i --name $CONT_NAME $VOLUMES extradata python zakupki/zakupki_companies_bs_range.py $DATA_DIR $PROC_DATE $CACHE_DIR $EMAIL $RNUM $RNUM_END_STEP
+		sudo docker rm -f $CONT_NAME
 
-sudo rm -r "../$CACHE_DIR"
-sudo cp /home/user/extradata/_data/* /home/smbuser/chroot/
+		sudo rm -r "../$CACHE_DIR"
+		sudo cp /home/user/extradata/_data/* /home/smbuser/chroot/
+
+		sleep 10s
+	done
